5a1ef92782be77ff4934788a36bb03de96c690a8
Pauliina <bridges.of.sighs@gmail.com>
2019-11-12 18:27:50 +0200

    validointi ja es lint

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+//käynnistys: node index.js
+
+require('dotenv').config() //sisältää tietokannan osoitteen ja salasanan
+const Person = require('./models/person') //Mongoose-moduulin käyttöönotto
+
 //Express on ohjelmointirajapintakirjasto, joka helpottaa palvelimen koodin tekemistä Node.js:llä
 const express = require('express')
 const app = express()
+app.use(express.static('build'))
 
 //body parser auttaa pääsemään käsiksi pyynnon mukana lähetettyyn dataan eli bodyyn
 const bodyParser = require('body-parser')
@@ -10,117 +16,93 @@ const cors = require('cors')
 
 app.use(cors())
 
-app.use(express.static('build'))
-
 //loggausta tekevä middleware
 const morgan = require('morgan')
 app.use(morgan('tiny'))
 
-let persons = [  
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },  
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },  
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ]
-
 //juuren tapahtumakäsittelijä
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World! :)</h1>')
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World! :)</h1>')
 })
 
 //infon tapahtumakäsittelijä
-app.get('/info', (req, res) => {
+app.get('/info', (request, response) => {
   const infos = persons.length
   const date = new Date()
-  res.send(`<p>Phonebook has info for ${infos} people</p> </p>${date}</p>`)
+  response.send(`<p>Phonebook has info for ${infos} people</p> </p>${date}</p>`)
 })
 
 //kaikkien resurssien haku
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons.map(person => person.toJSON()))
+  })
 })
 
 //yksittäisen resurssin haku
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {    
-      response.json(person)  
-  } 
-  else {    
-      response.status(404).end()  
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person.toJSON())
+      } else {
+        response.status(404).end() //olio ei löydy
+      }
+    })
+    .catch(error => next(error)) //id väärässä muodossa
 })
 
 //resurssin poisto
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error)) //virheenkäsittelyn siirtäminen middlewareen
 })
 
 //uusien puhelinnumerojen lähettäminen
-const generateId = (min, max) => {
-  return Math.floor(Math.random() * Math.floor(99999));
-}
-
-app.post('/api/persons', (request, response) => {
-  //käyttää body parseria
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  //nimi puuttuu
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
-  }
-
-  //nimi on jo luettelossa
-  const duplicate = persons.find(person => person.name === body.name)
-  if(duplicate) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  //numero puuttuu
-  if (!body.number) {
-    return response.status(400).json({ 
-      error: 'number missing' 
-    })
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    important: body.important || false,
-    id: generateId()
-  }
+    important: body.important || false
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save()
+  .then(savedPerson => savedPerson.toJSON()) //formatointi  
+  .then(savedAndFormattedPerson => {      
+    response.json(savedAndFormattedPerson)    
+  }) 
+  .catch(error => next(error)) //nimen ja numeron varmistus mongoosen sisäisellä validoinnilla
 })
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+//expressin virheidenkäsittelijä
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') { //virheellinen id-formaatti
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  else if (error.name === 'ValidationError') { //validointivirheet
+    return response.status(400).json({ error: error.message })  
+  }
+
+  next(error) //oletusarvoiselle virheenkäsittelijälle
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ models/person.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+const uniqueValidator = require('mongoose-unique-validator'); //estää duplikaatit
+
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true })
+    .then(result => {    
+      console.log('connected to MongoDB')  
+    })  
+    .catch((error) => {    
+        console.log('error connecting to MongoDB:', error.message)  
+    })
+
+//skeema määrittelee, millainen rakenne muistiinpanolla on
+//required: true - nimen ja numeron varmistus mongoosen sisäisellä validoinnilla
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true //tämän pitää olla uniikki
+  },
+  number: {
+    type: String,
+    required: true
+  },
+  important: Boolean,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+//estää duplikaatit
+personSchema.plugin(uniqueValidator, { message: 'name should be unique' });
+
+//kokoelmaan muistiinpanot tallennetaan nimellä person
+const Person = mongoose.model('Person', personSchema)
+
+module.exports = mongoose.model('Person', personSchema)
